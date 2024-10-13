84a2fa7d3c25aa364fd5892b17aa6fbc684fac2a
Macarena Garcia Quintans <macarenadgq@gmail.com>
2024-10-12 22:38:52 -0300

    agregado imprimir carnet y cambio hardcoded text

--- app/src/main/java/com/example/app_modelo/MenuPrincipal.kt
+++ app/src/main/java/com/example/app_modelo/MenuPrincipal.kt
@@ -4,21 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.app_modelo.R.id.btnEntregaCarnet
-import com.example.app_modelo.R.id.btnRegistrarSoc
+import com.example.app_modelo.R.id.btn_Volver
+import com.example.app_modelo.R.id.btn_emitir_vencimientos
 
 class MenuPrincipal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
 
-        val btnRegistarSocio = findViewById<Button>(btnRegistrarSoc)
+        val btnRegistarSocio = findViewById<Button>(btn_emitir_vencimientos)
         btnRegistarSocio.setOnClickListener{
             val intentarRegistro = Intent(this, Registrar_Socio_01::class.java)
             startActivity(intentarRegistro)
         }
 
-        val btnEntregaDeCarnet = findViewById<Button>(btnEntregaCarnet)
+        val btnEntregaDeCarnet = findViewById<Button>(btn_Volver)
         btnEntregaDeCarnet.setOnClickListener {
             val intentarEntregaCarnet = Intent(this, Entrega_Carnet_01::class.java)
             startActivity(intentarEntregaCarnet)
---
+++ app/src/main/java/com/example/app_modelo/activity_imprimir_carnet.kt
@@ -0,0 +1,11 @@
+package com.example.app_modelo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class activity_imprimir_carnet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imprimir_carnet)
+    }
+}
