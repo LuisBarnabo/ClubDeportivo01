db593b59f94ee76d19fd2e8398e3036ec65d9095
Macarena Garcia Quintans <macarenadgq@gmail.com>
2024-10-13 18:14:08 -0300

    agregado cobrar cuota y comprobante de pago de cuota

--- app/src/main/java/com/example/app_modelo/MenuPrincipal.kt
+++ app/src/main/java/com/example/app_modelo/MenuPrincipal.kt
@@ -4,21 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.app_modelo.R.id.btn_Volver
-import com.example.app_modelo.R.id.btn_emitir_vencimientos
+import com.example.app_modelo.R.id.btnEntregarCarnet
+import com.example.app_modelo.R.id.btnRegistrarSocio
 
 class MenuPrincipal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
 
-        val btnRegistarSocio = findViewById<Button>(btn_emitir_vencimientos)
+        val btnRegistarSocio = findViewById<Button>(btnRegistrarSocio)
         btnRegistarSocio.setOnClickListener{
             val intentarRegistro = Intent(this, Registrar_Socio_01::class.java)
             startActivity(intentarRegistro)
         }
 
-        val btnEntregaDeCarnet = findViewById<Button>(btn_Volver)
+        val btnEntregaDeCarnet = findViewById<Button>(btnEntregarCarnet)
         btnEntregaDeCarnet.setOnClickListener {
             val intentarEntregaCarnet = Intent(this, Entrega_Carnet_01::class.java)
             startActivity(intentarEntregaCarnet)
---
+++ app/src/main/java/com/example/app_modelo/activity_cobrar_cuota.kt
@@ -0,0 +1,20 @@
+package com.example.app_modelo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_cobrar_cuota : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cobrar_cuota)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
