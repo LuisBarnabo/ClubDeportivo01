80763b8230fb78235589c71c013b3c557cd2a22e
BusquetsLA <busquetsla@gmail.com>
2024-11-13 16:32:34 -0300

    vencimientos wip

--- app/src/main/java/com/example/app_modelo/ui/Vencimientos01.kt
+++ app/src/main/java/com/example/app_modelo/ui/Vencimientos01.kt
@@ -3,6 +3,7 @@ package com.example.app_modelo.ui
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.app_modelo.R
 import com.example.app_modelo.data.dao.PagosDao
@@ -11,16 +12,18 @@ import com.example.app_modelo.data.repository.PagosRepository
 class Vencimientos01 : AppCompatActivity() {
 
     private lateinit var pagosRepository: PagosRepository
+    private lateinit var lvVencimientos: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vencimientos01)
 
         pagosRepository = PagosRepository(this)
+        lvVencimientos = findViewById(R.id.lvVencimientos)
 
         val btnEmitir = findViewById<Button>(R.id.btn_EmitirVencimientos)
         btnEmitir.setOnClickListener {
-            pagosRepository.obtenerPagosVencidosHoy()
+            listarPagosVencidosHoy()
         }
 
         val btnVolver = findViewById<Button>(R.id.btn_Volver_Vencimientos)
@@ -28,5 +31,11 @@ class Vencimientos01 : AppCompatActivity() {
             val intentVolver = Intent(this, MenuPrincipal::class.java)
             startActivity(intentVolver)
         }
+    }
+
+    private fun listarPagosVencidosHoy() {
+        val pagosVencidosHoy = pagosRepository.obtenerPagosVencidosHoy()
+        val adapter = PagosAdapter(this, pagosVencidosHoy)
+        lvVencimientos.adapter = adapter
     }
 }
